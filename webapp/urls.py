673317c1ccc1f6c7a89b66149fd47f7b71e1f7e2
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2026-06-02 14:40:51 -0300

    Algoritmo

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import buscar_conexao_view
 
 urlpatterns = [
-    # Mapeia a URL /buscar para a função buscar_conexao_view
     path('buscar', buscar_conexao_view, name='buscar_conexao'),
 ]
